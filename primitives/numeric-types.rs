8c35b29adbad49e3f5946d33718bf44ab6e99e42
Hamza Eşsiz <webcatdev@gmail.com>
2023-01-23 20:32:32 +0300

    add hex int

--- primitives/numeric-types.rs
+++ primitives/numeric-types.rs
@@ -5,11 +5,15 @@ fn main() {
     // signed integer can store -2^(n-1) to 2^(n-1)-1
     // unsigned integer can store 0 to 2^(n-1)
     // u32 is default integer type
-    let a: u32 = 32;
+    let a = 32;
 
     // we can use types at the end of the integer to be explicit
     let b = 43u32;
 
-    println!("a = {a}");
-    println!("b = {b}");
+    // hex
+    let c: u64 = 0xF5A;
+
+    println!("a = {a} #default u32 type inference");
+    println!("b = {b} #decimal");
+    println!("c = {c} #hex")
 }
